5f0e38de84afb00b8ae99425533a3f7bcf166d6c
Boris Bronovitski <boris@bravia.ru>
2018-05-24 19:42:14 +0400

    remote control over tcp added to carousel

---
+++ src/todo.js
@@ -0,0 +1,12 @@
+//todo>> make carousel-based GUI library for electron.js
+//todo>> controller class
+//todo>> rewrite carousel and mbeegsrv
+//todo>> stream visualisation (dynamic charts for the beginning)
+//todo>> visualisation of inner data flows (for diagnostic and exploration purposes)
+//todo>> find data-flows sources (market data, stocks or bitcoin trading data for instance)
+//todo>> assemble an eeg sensor as a data source
+//todo>> find or write an SVM algorithm
+//todo>> add learning mode functionality to mbeeg
+//todo>> add learning mode functionality to visualisation tool
+//todo>> rename mbeeg to "gazer" or "watchdog"
+
